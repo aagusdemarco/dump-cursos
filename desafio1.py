8e382f687ba6d438858cf4cf55745edf441db85f
Agustin Demarco <123703934+aagusdemarco@users.noreply.github.com>
2023-09-23 10:12:15 -0300

    Add files via upload

---
+++ desafio1.py
@@ -0,0 +1,48 @@
+#Ejercicio 1
+
+nombre = 'Agustin'
+comidaFav = 'Canelones'
+
+print(f'Hola, mi nombre es {nombre} y mi comida favorita son los {comidaFav}')
+
+#Ejercicio 2
+
+a = 1
+b = 2.2
+c = False
+d = ['pedro', 'juan', 'raul']
+
+print(a, b, c, d)
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+
+#Ejercicio 3
+
+edad = '18'
+print('Hola, mi nombre es ' + nombre + ' y tengo ' + edad + ' años')
+
+#Ejercicio 4
+#Cuestionario -
+
+#Ejercicio 5
+
+Edad = 18
+Altura = 1.80
+Nombre = 'Agustin'
+Deporte = 'Futbol'
+Direccion = 'Calle Asborno'
+
+#Desafio 
+
+nombrePersonaje = 'Kakaroto'
+apodoPersonaje = 'Goku'
+profesionPersonaje = 'guerrero saiyajin'
+esposaPersonaje = 'Milk'
+hijoPersonaje = 'Gohan'
+alturaPersonaje = 1.75
+pesoPersonaje = 62
+nacimientoPersonaje = 737
+
+print(f'Su nombre es {nombrePersonaje}, aunque todos le dicen {apodoPersonaje}. Es un {profesionPersonaje}, nacido en {nacimientoPersonaje}, que mide {alturaPersonaje} metros y pesa {pesoPersonaje} kilos. Su esposa es {esposaPersonaje} y su hijo {hijoPersonaje}')
